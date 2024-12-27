d4fc3c518ed0ccd02d65ffe1e550e9cabafd726b
oop111703023 <111703023@g.nccu.edu.tw>
2024-12-28 00:19:32 +0800

    images and login

--- src/client/src/pages/Login/LoginPage.tsx
+++ src/client/src/pages/Login/LoginPage.tsx
@@ -1,19 +1,136 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+/* import witnessIcon from '../../assets/hide.png'; */
 
 const LoginPage: React.FC = () => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState<string>('');/* 存取錯誤訊息 */
+  const [currentImage, setCurrentImage] = useState('/assets/hide.png');
+
+  /* 驗證有效輸入 */
+  const validateInputs = (): string => {
+    if (!username) {
+      return 'Enter an username';
+    }
+    if (!password) {
+      return 'Enter a password';
+    }
+    if (password.length < 8) {
+      return 'Use 8 characters or more for your password';
+    }
+    return '';
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const validationError = validateInputs();
+    if (validationError) {
+      setError(validationError); /* 設置錯誤訊息 */
+      return;
+    }
+    setError(''); /* 清除錯誤訊息 */
+    console.log('登入資訊:', { username, password });
+  };
+
+  /* 獲取導航函數 */
+  const navigate = useNavigate(); 
+
+  /* 改變密碼圖示顯示狀態 */
+  const [passwordVisible, setPasswordVisible]=useState(false);
+  const [imageVisible, setImageVisible]=useState(false);
+
+  /* 切換密碼顯示或隱藏 */
+  const handleShowPassword = () => {
+    setPasswordVisible(true);
+    setImageVisible(true)
+    setTimeout(() => {
+      setPasswordVisible(false);
+      setCurrentImage('/assets/hide.png');
+    },1000);/* 設置為1sec後隱藏 */
+    setCurrentImage((prevImage) =>
+      prevImage === '/assets/hide.png' ? '/assets/witness.png' : '/assets/hide.png'
+    );
+  };
+  /* const handleShowPassword = () => {
+    setPasswordVisible(!passwordVisible); // 每次點擊切換顯示狀態
+  }; */
+
   return (
+    /* 定義一個容器<div> */
     <div className="container mt-5">
-      <h2 className="text-center text-primary mb-4">登入</h2>
-      <form className="mx-auto" style={{ maxWidth: '400px' }}>
+      <button
+          type="button"
+          className="btn btn-outline"
+          style={{ 
+            position: 'absolute',  // 固定定位
+            top: '10px',        // 頂部距離
+            left: '10px',       // 左側距離
+            zIndex: 1000 
+          }}
+          onClick={() => navigate('/')} // 跳回welcomePage
+        >
+          <img 
+                src="/assets/back.png"
+                style={{ 
+                  width: '24px', 
+                  height: '24px' 
+                }} 
+              />
+      </button>
+      <h2 className="text-start text-success fw-bold mb-4"
+      >Login</h2>
+      <form className="text-start" style={{ maxWidth: '400px' }} onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="username" className="form-label">使用者名稱</label>
-          <input type="text" className="form-control" id="username" placeholder="輸入您的使用者名稱" />
+          <label htmlFor="username" className="form-label">Username</label>
+          <input type="text" 
+            className="form-control" 
+            id="username" 
+            placeholder="Enter your username" 
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div className="mb-3">
-          <label htmlFor="password" className="form-label">密碼</label>
-          <input type="password" className="form-control" id="password" placeholder="輸入您的密碼" />
+          <label htmlFor="password" className="form-label">Password</label>
+          <div className="input-group">
+            <input
+              type={passwordVisible ? 'text' : 'password'} // 動態設置輸入框類型
+              className="form-control"
+              id="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              className="btn btn-outline-secondary" 
+              onClick={handleShowPassword} // 點擊切換顯示狀態
+            >
+              <img 
+                src={currentImage}
+                style={{ 
+                  width: '24px', 
+                  height: '24px' 
+                }} 
+              />
+              {/* {passwordVisible ? '隱藏' : '顯示'} */}
+            </button>
+          </div>
         </div>
-        <button type="submit" className="btn btn-success w-100">登入</button>
+        {error && <div 
+          className="btn alert alert-danger"
+          style={{
+            width: '400px',
+          }}
+        >
+        {error}
+        </div>} {/* 顯示錯誤訊息 */}
+        <button 
+          type="submit" 
+          className="btn btn-success w-100">Login
+        </button>
       </form>
     </div>
   );
--- src/client/src/pages/Login/SignUpPage.tsx
+++ src/client/src/pages/Login/SignUpPage.tsx
@@ -1,19 +1,152 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SignUpPage: React.FC = () => {
+
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [domain, setDomain] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState<string>('');/* 存取錯誤訊息 */
+  const [currentImage, setCurrentImage] = useState('/assets/hide.png');
+  
+  /* 驗證有效輸入 */
+  const validateInputs = (): string => {
+    if (!username) {
+      return 'Enter an username';
+    }
+    if (!password) {
+      return 'Enter a password';
+    }
+    if (password.length < 8) {
+      return 'Use 8 characters or more for your password';
+    }
+    return '';
+  };
+  
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const validationError = validateInputs();
+    if (validationError) {
+      setError(validationError); /* 設置錯誤訊息 */
+      return;
+    }
+    setError(''); /* 清除錯誤訊息 */
+    console.log('登入資訊:', { username, password });
+  };
+  /* 獲取導航函數 */
+  const navigate = useNavigate(); 
+
+  /* 改變密碼顯示狀態 */
+  const [passwordVisible, setPasswordVisible]=useState(false);
+  const [imageVisible, setImageVisible]=useState(false);
+  
+  /* 切換密碼顯示或隱藏 */
+  const handleShowPassword = () => {
+    setPasswordVisible(true);
+    setImageVisible(true)
+    setTimeout(() => {
+      setPasswordVisible(false);
+      setCurrentImage('/assets/hide.png');
+    },1000);/* 設置為1sec後隱藏 */
+    setCurrentImage((prevImage) =>
+      prevImage === '/assets/hide.png' ? '/assets/witness.png' : '/assets/hide.png'
+    );
+  };
+
   return (
     <div className="container mt-5">
-      <h2 className="text-center text-primary mb-4">註冊</h2>
-      <form className="mx-auto" style={{ maxWidth: '400px' }}>
+      <h2 className="text-start text-primary fw-bold mb-4">
+        Create an account</h2>
+      <form className="text-start" style={{ maxWidth: '400px' }}onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="username" className="form-label">使用者名稱</label>
-          <input type="text" className="form-control" id="username" placeholder="輸入您的使用者名稱" />
+          <label htmlFor="username" className="form-label">Username</label>
+          <input 
+            type="text" 
+            className="form-control" 
+            id="username" 
+            placeholder="Enter your username" 
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div className="mb-3">
-          <label htmlFor="password" className="form-label">密碼</label>
-          <input type="password" className="form-control" id="password" placeholder="輸入您的密碼" />
+          <label htmlFor="email" className="form-label">Email</label>
+          <div className='input-group'>
+            {/* 輸入框 */}
+            <input 
+                type="text" 
+                className="form-control" 
+                id="email" 
+                placeholder=" " 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            {/* @ */}
+            <span
+              className="input-group-text"
+              >
+                @
+            </span>
+            {/* 下拉式選單 */}
+            <select
+              className="form-select"
+              value={domain}
+              onChange={(e) => setDomain(e.target.value)}
+            >
+              <option value="gmail.com">gmail.com</option>
+              <option value="yahoo.com">yahoo.com</option>
+              <option value="outlook.com">outlook.com</option>
+              <option value="icloud.com">icloud.com</option>
+              <option value="custom.com">custom.com</option>
+            </select>
+          </div>
         </div>
-        <button type="submit" className="btn btn-success w-100">註冊</button>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <div className="input-group">
+            <input
+              type={passwordVisible ? 'text' : 'password'} // 動態設置輸入框類型
+              className="form-control"
+              id="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              className="btn btn-outline-secondary"
+              onClick={handleShowPassword} // 點擊切換顯示狀態
+            >
+              <img 
+                src={currentImage}
+                style={{ 
+                  width: '24px', 
+                  height: '24px' 
+                }} 
+              />
+            </button>
+          </div>
+        </div>
+        {error && (
+          <div 
+          className="btn alert alert-danger"
+          style={{
+            width: '400px',
+            /* overflowWrap='break-word', /*長字串自動換行*/
+          }}
+        >
+        {error}
+        </div>
+        )} {/* 顯示錯誤訊息 */}
+        <button type="submit" className="btn btn-primary w-100 mb-3">GET STARTED</button>
+        <button 
+          type="button" 
+          className="btn btn-light w-100"
+          onClick={() => navigate('/Login')}
+          >
+            Login
+        </button>
       </form>
     </div>
   );
